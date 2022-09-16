8fe9df0d9139ba401508f6fe78ef38a8d110180c
Nathássia Almeida <n.almeida.macedo@avanade.com>
2022-09-16 15:53:54 -0300

    first commit

---
+++ trabalho.ts
@@ -0,0 +1,140 @@
+let a = 1;
+
+for(let x = 0; x <= 10; x++){
+
+console.log(a, 'x', x, ' = ', a*x);
+
+}
+
+console.log("--------------");
+
+
+
+let b = 2;
+
+for(let x = 0; x <= 10; x++){
+
+console.log(b, 'x', x, ' = ', b*x);
+
+}
+
+console.log("--------------");
+
+
+
+let c = 3;
+
+for(let x = 0; x <= 10; x++){
+
+console.log(c, 'x', x, ' = ', c*x);
+
+}
+
+console.log("--------------");
+
+
+
+let d = 4;
+
+for(let x = 0; x <= 10; x++){
+
+console.log(d, 'x', x, ' = ', d*x);
+
+}
+
+console.log("--------------");
+
+
+
+let e = 5;
+
+for(let x = 0; x <= 10; x++){
+
+console.log(e, 'x', x, ' = ', e*x);
+
+}
+
+console.log("--------------");
+
+
+
+let f = 6;
+
+for(let x = 0; x <= 10; x++){
+
+console.log(f, 'x', x, ' = ', f*x);
+
+}
+
+console.log("--------------");
+
+
+
+let g = 7;
+
+for(let x = 0; x <= 10; x++){
+
+console.log(g, 'x', x, ' = ', g*x);
+
+}
+
+console.log("--------------");
+
+
+
+let h = 8;
+
+for(let x = 0; x <= 10; x++){
+
+console.log(h, 'x', x, ' = ', h*x);
+
+}
+
+console.log("--------------");
+
+
+
+let i = 9;
+
+for(let x = 0; x <= 10; x++){
+
+console.log(i, 'x', x, ' = ', i*x);
+
+}
+
+console.log("--------------");
+
+let j = 10;
+
+for(let x = 0; x <= 10; x++){
+
+console.log(j, 'x', x, ' = ', j*x);
+
+}
+
+console.log("--------------");
+
+
+
+/*Segue abaixo a resolução do segundo exercicio
+//Crie uma expressão regular onde o formato 123.456.abc.def seja válido,
+//porém 2123.456.abc.def também pode ser válido.
+
+/^[0-9]{3, 4}\.[0-9]{3}\.[a-z]{3}\.[a-z]{3}$/i;
+
+
+//CEP: 01311-100; 
+ /^[0-9]{5}\-[0-9]{3}$/;
+
+ //E-MAIL:
+ /^[a-z 0-9._] +@[a-z 0-9._]\.([a-z]+)?$/i;
+
+ //CPF: 
+ /^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}$/;
+
+// CNPJ: 
+/^[0-9]{2}\.[0-9]{3}\.[0-9]{3}\/[0-9]{4}\-[0-9]{2}$/;
+
+//RG: 22.455.213-2
+/^[0-9]{2}\.[0-9]{3}.[0-9]{3}\-[0-9]{2}/$/;
+*/
